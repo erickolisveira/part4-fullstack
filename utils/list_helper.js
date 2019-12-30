0505ef8b57691179c904bb3c88064fcebcd7aae2
Erick Oliveira <erickoliveiraah@gmail.com>
2019-12-30 17:11:42 -0300

    Exercise 4.5 completed

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, current) => sum + current.likes, 0) 
 }
 
+const favoriteBlog = (blogs) => {
+  const max = blogs.reduce((prev, curr) => {
+    return (prev.likes > curr.likes ? prev : curr)
+  }, 0)
+  const blog = {
+    title: max.title,
+    author: max.author,
+    likes: max.likes
+  }
+  return blog.likes !== undefined ? blog : max
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
 }
